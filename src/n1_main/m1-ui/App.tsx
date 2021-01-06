4154a9787d5a7c7958861cea901b2349c5d19668
Xiaomi <andriiyarotskiy@gmail.com>
2021-01-06 17:44:31 +0200

    change to BrowserRouter

--- src/n1_main/m1-ui/App.tsx
+++ src/n1_main/m1-ui/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {HashRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Header from "./component/Header/Header";
 import {Profile} from "./component/Profile/Profile";
 import AuthPage from "./pages/AuthPage";
@@ -10,14 +10,14 @@ import PasswordChange from "./component/PasswordChange/PasswordChange";
 const App = () => {
     return (
         <div className="App">
-            <HashRouter>
+            <BrowserRouter>
                 <Header/>
                 <Switch>
                     <Route exact path='/' render={() => <Profile/>}/>
                     <Route path='/authPage' render={() => <AuthPage/>}/>
                     <Route path='/passwordChange/:token' render={() => <PasswordChange/>}/>
                 </Switch>
-            </HashRouter>
+            </BrowserRouter>
         </div>
     );
 }
